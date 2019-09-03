07621d322a47be96a27f84b545a8336ab928687e
blackeyes1023 <525277229@qq.com>
2019-09-03 16:37:42 +0800

    实现表单模板、重定向及用户会话功能

--- weibo/weibo.py
+++ weibo/weibo.py
@@ -1,18 +1,39 @@
 
 from datetime import datetime
 
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, session, flash
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "hard to guess string"
+
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
 
-@app.route("/")
+class NameForm(FlaskForm):
+	name = StringField("what's your name?", validators=[DataRequired()])
+	submit = SubmitField("submit")
+
+
+@app.route("/", methods=["POST", "GET"])
 def index():
-	return render_template("index.html", current_time=datetime.utcnow())
+	form = NameForm()
+	if form.validate_on_submit():
+		old_name = session.get("name")
+		if old_name is not None and old_name != form.name.data:
+			flash("Look like you have changed name.")
+		session["name"] = form.name.data
+		form.name.data = ""
+		return redirect(url_for("index"))
+	return render_template("index.html",
+	                       current_time=datetime.utcnow(),
+	                       form=form,
+	                       name=session.get("name"))
 
 
 @app.errorhandler(404)
